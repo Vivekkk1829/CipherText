450f293dc27598454e9459bcc984ed25950280d8
Vivekkk1829 <vivekkatta1829@gmail.com>
2025-12-24 16:43:32 +0530

    Middleware error

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -24,6 +24,7 @@ const authMiddleware = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
+     console.log(error)
     return res.status(401).json({
       success: false,
       message: "Invalid token",
